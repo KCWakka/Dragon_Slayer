20ed8fc2d1439c473c79bd7cdbc204134418a8c7
BT_2N2_11 <sample@gmail.com>
2024-01-05 14:04:23 -0500

    added choices.

--- src/DragonSlayer.java
+++ src/DragonSlayer.java
@@ -20,7 +20,7 @@ public class DragonSlayer {
     }
 
     private void startingGame() {
-        System.out.println("Welcome to the Dragon Slayer game where the goal is to defeat the dungeon by defeat the dragon in each room the dugneon has.");
+        System.out.println( Colors.CYAN +"Welcome to the Dragon Slayer game where the goal is to defeat the dungeon by defeat the dragon in each room the dugneon has." + Colors.RESET);
         System.out.println("Some dragon are tougher are other so you can achieve your goals by scavenging for resources and upgrading your sword!");
         System.out.print("Enter player's name: ");
         String name = scan.nextLine();
@@ -38,6 +38,17 @@ public class DragonSlayer {
         while (!choice.equals("g")) {
             System.out.println();
             System.out.println(currentRoom.getPrintMessage());
+            System.out.println("***");
+            System.out.println(player);
+            System.out.println(currentRoom);
+            System.out.println("(E)nter the next room.");
+            System.out.println("(S)earch the room.");
+            System.out.println("(F)ight the dragon.");
+            System.out.println("(I)nspect the dragon.");
+            System.out.println("(U)se health pot.(Only if you have one)");
+            System.out.println("(L)ook at your weapon's stat.");
+            System.out.println("(B)uy an weapon upgrade!");
+            choice = scan.nextLine().toLowerCase();
         }
     }
 
